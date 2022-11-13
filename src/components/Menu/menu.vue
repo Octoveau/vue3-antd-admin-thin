d8b0b792a88a6f0a12589d8416e1c87703c4ed8f
cicada_fu <28926044+zero-fsc@users.noreply.github.com>
2022-11-13 21:30:09 +0800

    fix menu bug

--- src/components/Menu/menu.vue
+++ src/components/Menu/menu.vue
@@ -1,30 +1,56 @@
 <template>
   <a-menu
+    v-model:openKeys="openKeys"
     v-model:selectedKeys="selectedKeys"
     mode="inline"
+    @openChange="handleOpenChange"
   >
     <menu-item :menuList="props.menuList" />
   </a-menu>
 </template>
 
 <script lang="ts" setup>
-import MenuItem from './menuItem.vue'
+import MenuItem from './menuItem.vue';
 import { PropType } from 'vue';
-import { MenuListInterface } from './menu.interface'
+import { MenuListInterface } from './menu.interface';
 import { useRoute } from 'vue-router';
-const $route = useRoute()
+const $route = useRoute();
 const props = defineProps({
   menuList: {
     type: Array as PropType<MenuListInterface[]>,
     default: () => []
   }
-})
+});
+
+const openKeys = ref<any>();
+
+onMounted(() => {
+  openKeys.value = [getOpenKey(props.menuList)?.menu_name];
+});
+
 const selectedKeys = computed(() => {
-  const { path } = $route
+  const { path } = $route;
   if (path) {
-    return [path]
+    return [path];
   }
 
-  return ['DashBoard']
-})
+  return ['DashBoard'];
+});
+
+const getOpenKey = (menuList: any) => {
+  const { path } = $route;
+  let fatherItem: any;
+  for (let item of menuList) {
+    if (item.menu_url === path) {
+      return;
+    }
+    if (item.children && item.children.length) {
+      fatherItem = item;
+      getOpenKey(item.children);
+    }
+  }
+  return fatherItem;
+};
+
+const handleOpenChange = (keys: string[]) => openKeys.value = keys.length !== 0 ? [keys[1]] : [''];
 </script>
--- src/mock/menu.mock.js
+++ src/mock/menu.mock.js
@@ -2,11 +2,47 @@ export const menuList = [
   {
     menu_name: 'DashBoard',
     menu_url: '/dashboard',
-    menu_icon: 'MailOutlined',
+    menu_icon: 'el-icon-s-home',
   },
   {
-    menu_name: 'API模块',
-    menu_url: '/apilist',
-    menu_icon: 'fast-backward-outlined',
+    menu_name: '嵌套菜单',
+    menu_icon: 'el-icon-connection',
+    children: [
+      {
+        menu_name: '菜单1',
+        menu_url: '',
+        children: [
+          {
+            menu_name: '菜单1-1',
+            menu_url: '',
+            children: [
+              {
+                menu_name: '菜单1-1-1',
+                menu_url: ''
+              }
+            ]
+          },
+          {
+            menu_name: '菜单1-2',
+            menu_url: ''
+          }
+        ]
+      },
+      {
+        menu_name: '菜单2',
+        menu_url: ''
+      }
+    ]
   },
+  {
+    menu_name: '地图模块',
+    menu_url: '',
+    menu_icon: 'iconfont yl-menu-icon icon-ditu',
+    children: [
+      {
+        menu_name: '百度地图',
+        menu_url: '/apilist'
+      },
+    ]
+  }
 ]
